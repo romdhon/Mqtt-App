2dbfdaa307163f41848603c3104064bebcc1d15b
romdhon <romdhon.1998@gmail.com>
2020-05-12 10:42:05 +0700

    'second'

--- mqtt/main.py
+++ mqtt/main.py
@@ -1,11 +1,35 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from py.setting import Display
+from py.setting import Display, StatusLabel
 import paho.mqtt.client as mqtt
 
 class MainApp(MDApp):
+    mqttc = mqtt.Client()
+    host = 'broker.mqttdashboard.com'
+
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         return Builder.load_file(f'kv/main.kv')
+
+    def on_connect(self, *args):
+        self.root.ids.tb.title = \
+            'MQTT Connected'
+
+    def on_message_ph(self, client, userdata, msg):
+        message = msg.payload.decode('utf-8', 'strict')
+        self.root.ids.stat1.text = '{}'.format(message)
+
+    def on_message_temp(self, client, userdata, msg):
+        message = msg.payload.decode('utf-8', 'strict')
+        self.root.ids.stat2.text = '{}'.format(message)
+
+    def on_start(self):
+        
+        self.mqttc.connect(self.host)
+        self.mqttc.on_connect = self.on_connect
+        self.mqttc.message_callback_add('sensors_out/ph', self.on_message_ph)
+        self.mqttc.message_callback_add('sensors_out/temp', self.on_message_temp)
+        self.mqttc.subscribe('sensors_out/#', 0)
+        self.mqttc.loop_start()
 
 MainApp().run()
--- mqtt/py/setting.py
+++ mqtt/py/setting.py
@@ -3,6 +3,8 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.button import MDRoundFlatIconButton
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
+from kivy.properties import StringProperty, NumericProperty
+from kivymd.app import MDApp
 
 class Display(MDCard):
     pass
@@ -10,8 +12,41 @@ class Display(MDCard):
 class MyLabel(MDLabel):
     pass
 
-class RoundButton(MDRoundFlatIconButton):
-    pass
+class StatusLabel(MDLabel):
+    ph = StringProperty('PH Sensor')
+    temp = StringProperty('Temp Sensor')
 
 class ImageButton(ButtonBehavior, Image):
-    pass
+    button_off = StringProperty('buttons/off.png')
+    button_on = StringProperty('buttons/on.png')
+
+    num_bt1 = NumericProperty(0)
+    num_bt2 = NumericProperty(2)
+
+    def change_mode(self, num):
+        if self.source == self.button_off:
+            self.source = self.button_on
+            num = 1
+        else:
+            self.source = self.button_off
+            num = 0
+        print(num)
+        mdapp = MDApp.get_running_app()
+        mdapp.mqttc.publish('sensors_in/moisture', num)
+
+    def on_off_motor(self, num):
+        if self.source == self.button_off:
+            self.source = self.button_on
+            num = 3
+        else:
+            self.source = self.button_off
+            num = 2
+        print(num)
+        mdapp = MDApp.get_running_app()
+        mdapp.mqttc.publish('sensors_in/moisture', num)
+
+
+        
+
+
+    
