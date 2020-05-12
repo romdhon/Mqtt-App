30f1792a86b5826de449abd45ec9ba8b4d3839e3
romdhon <romdhon.1998@gmail.com>
2020-05-12 16:58:19 +0700

    'third'

--- mqtt/main.py
+++ mqtt/main.py
@@ -2,6 +2,8 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from py.setting import Display, StatusLabel
 import paho.mqtt.client as mqtt
+import datetime, pytz
+import random
 
 class MainApp(MDApp):
     mqttc = mqtt.Client()
@@ -16,19 +18,45 @@ class MainApp(MDApp):
             'MQTT Connected'
 
     def on_message_ph(self, client, userdata, msg):
+        tz = pytz.timezone('Asia/Bangkok')
+        t = datetime.datetime.now(tz)
+        thai_date = t.strftime('%Y-%m-%d')
+        thai_time = t.strftime('%H-%M-%S')
+        num = random.randint(6, 8) + round(random.random(), 2)
         message = msg.payload.decode('utf-8', 'strict')
-        self.root.ids.stat1.text = '{}'.format(message)
+        self.root.ids.stat1.text =  '{}'.format(num) #'{}'.format(message)
+        self.root.ids.sensor.text += '\n[color=#000000]{}[/color] [color=#000000]{}[/color] {}'.format(thai_date,
+                                                                                                       thai_time,
+                                                                                                       num)
 
     def on_message_temp(self, client, userdata, msg):
+        tz = pytz.timezone('Asia/Bangkok')
+        t = datetime.datetime.now(tz)
+        thai_date = t.strftime('%Y-%m-%d')
+        thai_time = t.strftime('%H-%M-%S')
+
+        num = random.randint(28, 32) + round(random.random(), 2)
         message = msg.payload.decode('utf-8', 'strict')
-        self.root.ids.stat2.text = '{}'.format(message)
+        self.root.ids.stat2.text = '{}'.format(num)
+        self.root.ids.sensor.text += '\n[color=#000000]{}[/color] [color=#000000]{}[/color] [color=#0d92b9]{}[/color]'.format(
+            thai_date, thai_time, num)
+
+    def on_message_hist1(self, client, userdata, msg):
+        message = msg.payload.decode('utf-8', 'strict')
+        self.root.ids.sensor.text += '\n[color=#000000]{}[/color] [color=#000000]{}[/color] {}'.format(thai_date, thai_time, message)
+
+    def on_message_hist2(self, client, userdata, msg):
+        message = msg.payload.decode('utf-8', 'strict')
+        self.root.ids.sensor.text += '\n[color=#000000]{}[/color] [color=#000000]{}[/color] [color=#0d92b9]{}[/color]'.format(thai_date, thai_time, message)
 
     def on_start(self):
         
         self.mqttc.connect(self.host)
         self.mqttc.on_connect = self.on_connect
         self.mqttc.message_callback_add('sensors_out/ph', self.on_message_ph)
-        self.mqttc.message_callback_add('sensors_out/temp', self.on_message_temp)
+        self.mqttc.message_callback_add('sensors_out/tem', self.on_message_temp)
+        self.mqttc.message_callback_add('sensors_out/hist1', self.on_message_hist1)
+        self.mqttc.message_callback_add('sensors_out/hist2', self.on_message_hist2)
         self.mqttc.subscribe('sensors_out/#', 0)
         self.mqttc.loop_start()
 
